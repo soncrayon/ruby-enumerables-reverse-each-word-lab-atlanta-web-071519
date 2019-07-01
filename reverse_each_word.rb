7b458907a108e9a01d42ae4800ffa698553fa007
Curtis Arnold <arnold.curtis@gmail.com>
2019-07-01 00:46:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(string)
   word_array = string.split(",")
   reverse_word_array = word_array.map {|s| s.reverse!}
-  
+  reverse_word_phrase = reverse_word_array.join("")
+  return reverse_word_phrase
 end 
